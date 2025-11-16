3d0e59077ed5b45e5186b96bcce8253b3ba6118d
Illia-pro2025 <illamyrovec05@gmail.com>
2025-11-16 14:02:37 +0100

    changes

---
+++ galery.js
@@ -0,0 +1,10 @@
+const slides = document.querySelectorAll('.bibliothek__item--gallery active');
+let slideIndex = 0;
+slides.forEach((slide, index) => {
+  slide.addEventListener('click', () => {
+    slides[slideIndex].classList.remove('active');
+    slideIndex = (index + 1) % slides.length;
+    slides[slideIndex].classList.add('active');
+    console.log('-');
+  });
+});
